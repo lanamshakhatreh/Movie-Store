ecf02794a4a4c126857911faee4218aef643b26f
Lana Shakhatreh <34197175+lanamshakhatreh@users.noreply.github.com>
2019-01-28 03:11:03 +0200

    Add files via upload

---
+++ MovieStore.WebUI/MovieStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MovieStore.Domain.Entities;
+
+namespace MovieStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+    }
+}
