dfe4ea8c485be816570fcaef76c7a224eaac185e
Lana Shakhatreh <34197175+lanamshakhatreh@users.noreply.github.com>
2019-01-28 02:47:23 +0200

    Add files via upload

---
+++ MovieStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MovieStore.Domain.Entities;
+
+namespace MovieStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
